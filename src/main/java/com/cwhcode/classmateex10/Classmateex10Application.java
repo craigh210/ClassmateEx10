f232e9b8802aebd79aff01a5ad2002aa7cda1f49
craigh210 <craighunt210@gmail.com>
2019-04-07 18:21:31 -0400

    Initial commit

---
+++ src/main/java/com/cwhcode/classmateex10/Classmateex10Application.java
@@ -0,0 +1,13 @@
+package com.cwhcode.classmateex10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classmateex10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Classmateex10Application.class, args);
+    }
+
+}
